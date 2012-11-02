75f24aea3383e61afcf793e44770857bf9cb3649
xcezx <main.xcezx@gmail.com>
2012-11-02 11:46:49 +0900

    first commit

---
+++ metadata.rb
@@ -0,0 +1,16 @@
+maintainer       "MAEKAWA Tsuyoshi"
+maintainer_email "main.xcezx@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures php-redis"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends "php"
+depends "git"
+depends "build-essential"
+
+%{ debian ubuntu }.each do |os|
+  supports os
+end
+
+recipe "php-redis", "Install redis extension from source."
---
+++ recipes/default.rb
@@ -0,0 +1,40 @@
+#
+# Cookbook Name:: php-redis
+# Recipe:: default
+#
+
+include_recipe "php"
+include_recipe "git"
+include_recipe "build-essential"
+
+git "/usr/local/src/phpredis" do
+  repository "git://github.com/nicolasff/phpredis.git"
+  reference "master"
+  action :sync
+end
+
+execute "phpize" do
+  command "phpize"
+  cwd "/usr/local/src/phpredis"
+end
+
+execute "configure" do
+  command "./configure"
+  cwd "/usr/local/src/phpredis"
+end
+
+execute "make" do
+  command "make"
+  cwd "/usr/local/src/phpredis"
+end
+
+execute "make install" do
+  command "make install"
+  cwd "/usr/local/src/phpredis"
+end
+
+cookbook_file "/etc/php5/conf.d/redis.ini" do
+  source "redis.ini"
+  action :create_if_missing
+end
+
